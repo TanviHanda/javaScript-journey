41a47689a6e3f2c9025c601070ef5897fad09786
TanviHanda <tanvihanda66@gmail.com>
2025-07-07 02:19:33 +0000

    conversion of datatypes

---
+++ 01_basics/03_DatatypesConversion.js
@@ -0,0 +1,19 @@
+// let score = "3abc3"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); 
+
+//Null -> 0
+//"3abc3" -> NaN
+//true -> 1; false -> 0
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// "" -> false
+// "tanvi" -> true
