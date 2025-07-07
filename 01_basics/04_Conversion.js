4d18f27ddf1167fc20f74558b0d12b6f71da59a7
TanviHanda <tanvihanda66@gmail.com>
2025-07-07 03:27:45 +0000

    conversion and datatypesummary

---
+++ 01_basics/04_Conversion.js
@@ -0,0 +1,23 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// here js convert string into number
+// console.log("2">1); //true
+// console.log("02">1); //true
+
+//js converts null into 0
+// console.log(null > 0); //F
+// console.log(null == 0); //F
+// console.log(null >= 0); //T
+
+// console.log(undefined == 0); //F
+// console.log(undefined > 0); //F
+// console.log(undefined < 0); //F
+
+// ===
+// console.log("2"===2); //false -> it is use for strict checking
+// console.log("2"==2); //true -> it's donot compare types like string etc.
+
---
+++ 01_basics/05_DatatypesSummary.js
@@ -0,0 +1,44 @@
+// # primitive
+// 7 categories : String , Number , Boolean , null , undefined , Symbol , BigInt
+
+// const score = 100
+// const scoreValue = 100.3
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail
+// console.log(score,scoreValue,isLoggedIn,outsideTemp,userEmail);
+//output=>
+//100 100.3 false null undefined
+
+// //Symbol is used for uniqueness
+// const id = Symbol('123')
+// const id2 = Symbol('123')
+// console.log(id,id2);
+// console.log(id === id2); //false
+
+// const bigNumber = 33468486787685435212346n
+// console.log(bigNumber);
+
+// ------------------------------------
+// # reference / non-primitive 
+// Array , Objects , Functions
+// const fruitsArr = ["cherry","mango","apple"];
+// let myObj = {
+//     name: "tanvi",
+//     age: 18
+// }
+// const myFunction = function(){
+//     console.log("hello world");   
+// }
+// -----------------------------------Note
+//JavaScript is a dynamically typed language.
+// This means that:
+// Type checking occurs at runtime.
+// No explicit type declarations are required.
+// const num = 100
+// Variable types can change:
+// let myVariable = 10; // myVariable is a number
+// myVariable = "hello"; // myVariable is now a string
+
+// link
+// http://262.ecma-international.org/5.1/#sec-11.4.3
