26f4b7bf8a8b97de80d756c977adbe72443166e5
TanviHanda <tanvihanda66@gmail.com>
2025-07-18 07:28:13 +0000

    this keyword and arrow function

---
+++ 03_basics/03_thisAndArrow.js
@@ -0,0 +1,17 @@
+//------this keyword
+
+// const user = {
+//     productname:'OPPO A79',
+//     price:999,
+//     welcomeMessage : function(){
+//         console.log(`${this.productname}`);
+        
+//     }
+// }
+// user.welcomeMessage()
+
+//------Arrow function 
+// const Sum = (a,b)=>{
+//     return a+b;
+// }
+// console.log(Sum(1,3)); //4
