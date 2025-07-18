f600c31ca10c01b3520ff171fe126814639e0a17
TanviHanda <tanvihanda66@gmail.com>
2025-07-18 08:09:54 +0000

    IIFE function

---
+++ 03_basics/04_IIFE_function.js
@@ -0,0 +1,10 @@
+// global scope ke pollution se problem hoti h isliye iifi ko use krte h
+// (function greet(){
+//     console.log('hello');
+// })(); 
+// (function greet(){
+//     console.log('hello');
+// })();
+// ((name )=>{
+//     console.log(`hello ${name}`);
+// })('tanvi')
